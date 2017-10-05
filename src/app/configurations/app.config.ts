343a8e0a4273e32b1e94c53f98a9a02e0c2c17e7
rprabhu0208 <rprabhu0208@gmail.com>
2017-10-05 23:11:26 +0530

    10052017

--- src/app/configurations/app.config.ts
+++ src/app/configurations/app.config.ts
@@ -1,7 +1,8 @@
 
 export const AppConfig = {
     "Services" : {
-        "baseUrl" : "http://172.17.40.142"
-        //  "baseUrl" : "http://localhost:5000"
+       // "baseUrl" : "http://172.17.40.142"
+       "baseUrl" : "http://localhost:5000",
+       "metaDataUrl" : "http://localhost:5001"
     }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -21,7 +21,8 @@ export class DataService
          return new Promise(
             (resolve,reject) =>
         {   
-            this.HTTPService.get(AppConfig.Services.baseUrl+"/metadata/api/metadata")
+            this.HTTPService.get(AppConfig.Services.metaDataUrl+"/metadata/api/metadata")
+            //this.HTTPService.get(AppConfig.Services.metaDataUrl + "/metadata")
             .subscribe(
                 (response) => {  
                 
@@ -59,13 +60,15 @@ export class DataService
             //     }
             // )
 
-            this.HTTPService.get(AppConfig.Services.baseUrl+"/infinity/api/policysearch",params)
+            // this.HTTPService.get(AppConfig.Services.baseUrl+"/infinity/api/policysearch",params)
+            this.HTTPService.get(AppConfig.Services.baseUrl+"/infinity/api/policysearch")
             .subscribe(
                 (response) => {  
-                    let policyResults = response.json();
+                    debugger;
+                    //let policyResults = response.json();
 
-                    // this.PolicySearchResults = response.json();
-                    this.PolicySearchResults = policyResults.policySearchResults;
+                     this.PolicySearchResults = response.json();
+                    //this.PolicySearchResults = policyResults.policySearchResults;
                     return resolve(this.PolicySearchResults); 
                 },
                 (error) => {
